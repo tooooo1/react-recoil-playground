d7f1ab18c02e6226e0656fb35350ce00745ccd59
tooooo1 <wer4272@nate.com>
2023-01-03 01:19:15 +0900

    feat: 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,24 @@
+import React from 'react';
+import { atom, useRecoilState } from 'recoil';
+
+const devState = atom({
+  key: 'devState',
+  default: '',
+});
+
 const App = () => {
-  return <div></div>;
+  const [dev, setDev] = useRecoilState(devState);
+
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setDev(event.target.value);
+  };
+
+  return (
+    <>
+      <input type="text" value={dev} onChange={onChange} />
+      <br /> {dev}
+    </>
+  );
 };
 
 export default App;
